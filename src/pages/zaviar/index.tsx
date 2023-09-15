df63d49c0f06fd6b52b1897fe5f3a6f7362acb70
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-09-15 11:42:55 -0700

    🆕 created new 3d scene for zav profile 🆕

--- src/pages/zaviar/index.tsx
+++ src/pages/zaviar/index.tsx
@@ -1,8 +1,24 @@
-export default function StevenAndersonProfile() {
-    return (
-      <>
-        <div>zavitar420</div>
-      </>
-    );
-  }
-  
+import Spline from "@splinetool/react-spline";
+import { motion } from "framer-motion";
+
+export default function ZaviarBrownProfile() {
+  return (
+    <div className="mt-20 flex w-screen items-center justify-center ">
+      <motion.div
+        initial={{ opacity: 1, x: -200 }}
+        animate={{
+          opacity: 1,
+          x: 0,
+        }}
+        transition={{
+          duration: 3,
+          type: "spring",
+          stiffness: 75,
+          damping: 4
+        }}
+      >
+        <Spline scene="https://prod.spline.design/rlJs00XkpmgtyfSU/scene.splinecode" />
+      </motion.div>
+    </div>
+  );
+}

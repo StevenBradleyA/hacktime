7c758b413bf9008d790f487034a0d62437450b65
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-11-17 13:13:47 -0800

    🔥 added new create request route 🔥

--- src/server/api/routers/request.ts
+++ src/server/api/routers/request.ts
@@ -15,6 +15,23 @@ export const requestRouter = createTRPCRouter({
     return "you can now see this secret message!";
   }),
 
+  create: publicProcedure
+    .input(
+      z.object({
+        budget: z.string(),
+        email: z.string(),
+        text: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { budget, email, text } = input;
+      const newRequest = await ctx.prisma.request.create({
+        data: { budget, email, text },
+      });
+
+      return newRequest;
+    }),
+
   // want to create a route that returns true or false depending on if the secret password matches
   // if it does we will show all requests will be saved in cookies
 

1bd4c95545b842e74508318c525fadcdeb393b4c
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-11-17 12:16:47 -0800

    📚 added info to contact form 📚

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,11 +1,50 @@
+import { motion } from "framer-motion";
+
 interface ContactFormProps {
   closeModal: () => void;
 }
 
 export default function ContactForm({ closeModal }: ContactFormProps) {
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div className="flex-col ">
+      <div>{`Let's work Together!`}</div>
+      <div>what is your budget? </div>
+      <div className="flex">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          className="rounded-3xl bg-white px-6 py-2"
+        >
+          0-1k
+        </motion.button>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          className="rounded-3xl bg-white px-6 py-2"
+        >
+          1-5k
+        </motion.button>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          className="rounded-3xl bg-white px-6 py-2"
+        >
+          10k+
+        </motion.button>
+      </div>
+
+      <div>What kind of site do you want us to build for you?</div>
+      <input />
+
+      <div>{`What's your email?`} </div>
+      <input />
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+        className="rounded-3xl bg-white px-6 py-2"
+      >
+        Submit
+      </motion.button>
+    </div>
   );
 }
--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -2,6 +2,13 @@ export default function Contact() {
   return (
     <div>
       <div className="text-9xl">CONTACT US</div>
+
+      <div className="mt-96"> Reach us at </div>
+      <div> hacktimedev@outlook.com</div>
+
+      <div> contact form</div>
+
+      <div> links to our linkedIn as well</div>
     </div>
   );
 }

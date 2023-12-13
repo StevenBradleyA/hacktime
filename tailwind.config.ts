6ed4e92a75ab47de2a383284207b543a9d5cac00
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-12-13 10:02:43 -0800

    🔥 updated config with new backgrounds  🔥

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
         neonTeal: "#00FF99",
         hack: "#00dd00",
         failure: "#ff0000",
+        graybae: "#adadad",
+        chillWhite: "#f1f1ef",
       },
       backgroundColor: {
         dark: "#1E1E1E",
@@ -18,6 +20,8 @@ export default {
         slateBlack: "#202124",
         charcoal: "#333",
         gold: "#D4AF37",
+        lessDark: "#101010",
+        offDark: "#2f2f2f",
       },
       screens: {
         mobile: "320px",

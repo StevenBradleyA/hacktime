267daba3299cc2e3b6e9d756488095daebb151e1
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-12-15 16:40:32 -0800

    🔥 admin page now retro-futuristic themed 🔥

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -6,7 +6,9 @@ import Link from "next/link";
 import projectLogo from "@public/Admin/projects.png";
 import anonymous from "@public/Admin/admin-green.png";
 import adminTitle from "@public/Admin/admin-vert-title.png";
-import diagonal from "@public/Graphics/diagonal-1.png"
+import diagonal from "@public/Graphics/diagonal-1.png";
+import dots from "@public/Graphics/dots-rect.png";
+import smile from "@public/Graphics/smile-blur.png";
 import { useState } from "react";
 import CrossIcon from "~/components/Icons/cross";
 import SkullCrossBones from "~/components/Icons/skullCrossBones";
@@ -40,35 +42,44 @@ export default function Admin() {
 
   return (
     <>
-      <div className="relative h-10 w-1/3">
+      <div className="relative w-1/3">
         <Image
           src={adminTitle}
           alt="admin title"
-          className="png-light-gray absolute -left-40"
+          className="png-light-gray absolute -left-44"
         />
       </div>
 
-      <div className="relative w-full">
+      <div className="relative w-full ">
         <Image
           src={anonymous}
           alt="admin logo"
-          className="absolute left-20 top-0 w-44 "
+          className="absolute left-20 top-20 w-44 "
         />
         <Image
           src={diagonal}
           alt="admin logo"
-          className="absolute left-20 top-44 w-44 "
-
+          className="absolute left-20 top-64 w-44 "
         />
       </div>
+
+      <div className="relative w-full "></div>
 
       <div className="relative mb-10 flex w-full">
         <Link href="/admin/projects" aria-label="admin projects">
           <Image
-            src={projectLogo}
+            src={dots}
             alt="projects"
-            className="absolute right-32 w-96"
+            className="absolute right-32 top-44 w-72"
           />
+          <Image
+            src={smile}
+            alt="admin logo"
+            className="png-light-gray absolute right-20 top-20 w-44 "
+          />
+          <div className=" absolute right-56 top-64 w-44 text-6xl ">
+            PROJECTS
+          </div>
         </Link>
         <button className="absolute right-72 top-0"></button>
       </div>
@@ -78,35 +89,24 @@ export default function Admin() {
           <div key={i} className="w-1/3">
             {!deleteConfirmation && (
               <motion.button
-                className="flex w-full justify-end text-orange-400 "
+                className="flex w-full justify-end  "
                 onClick={() => setDeleteConfirmation(true)}
-                whileHover={{
-                  y: -2,
-                  transition: { type: "spring", stiffness: 400 },
-                }}
-                whileTap={{ scale: 0.95 }}
               >
                 <CrossIcon />
               </motion.button>
             )}
             {deleteConfirmation && (
-              <div className="flex w-full justify-end gap-5 text-orange-400">
+              <div className="flex w-full justify-end gap-5 ">
                 <motion.button
                   onClick={() => mutate({ id: e.id })}
-                  whileHover={{
-                    y: -2,
-                    transition: { type: "spring", stiffness: 400 },
-                  }}
+                  whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.95 }}
                 >
                   <SkullCrossBones />
                 </motion.button>
                 <motion.button
                   onClick={() => setDeleteConfirmation(false)}
-                  whileHover={{
-                    y: -2,
-                    transition: { type: "spring", stiffness: 400 },
-                  }}
+                  whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.95 }}
                 >
                   {`C:// abort`}
@@ -116,8 +116,8 @@ export default function Admin() {
 
             <div className="relative mb-10 mt-1  rounded-xl bg-black p-10 text-white">
               <div className="mb-5 flex justify-between">
-                <div className="text-sky-300">{e.email}</div>
-                <div className="text-pink-300">{e.budget}</div>
+                <div className="text-green-500">{e.email}</div>
+                <div className="text-hackGray">{e.budget}</div>
               </div>
               <div>{e.text}</div>
             </div>
